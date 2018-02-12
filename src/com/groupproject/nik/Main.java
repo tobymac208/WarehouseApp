2c1869238c66a3685fe5f96910281fe9adb69a94
Nik <ironman_fern@comcast.net>
2018-02-11 19:13:42 -0600

    Added objects to the main screen for display. Added styling for the windows.

--- src/com/groupproject/nik/Main.java
+++ src/com/groupproject/nik/Main.java
@@ -1,11 +1,13 @@
 package com.groupproject.nik;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -17,8 +19,8 @@ import javafx.stage.Stage;
 public class Main extends Application {
     // Change these values to change the window's values
     private String windowTitle = "Login";
-    private final int WIDTH = 600; // sets the window's width
-    private final int HEIGHT = 400; // sets the window's height
+    private final double WIDTH = 600; // sets the window's width
+    private final double HEIGHT = 400; // sets the window's height
 
     public static void main(String[] args) {
         launch(args);
@@ -39,13 +41,32 @@ public class Main extends Application {
         Button login = new Button("Login");
 
         // Events -- the place to add new functionality when the user clicks an object
+        stage.setOnCloseRequest(event -> stage.close()); // tells the window to close completely when you hit the X (quit) button
         // TODO: Hook up functionality for logging in :: Required component: An account Model for holding user info
 
         // Layout -- the place to put all of the objects
         BorderPane layout = new BorderPane();
+        // CENTER
+        GridPane centerLayout = new GridPane();
+        centerLayout.setHgap(10); // sets the horizontal gap
+        centerLayout.setVgap(10); // sets the vertical gap
+        GridPane.setConstraints(usernameLabel, 0, 0); // first row, first column
+        GridPane.setConstraints(usernameField, 1, 0); // first row, second column
+        GridPane.setConstraints(passwordLabel, 0, 1); // second row, first column
+        GridPane.setConstraints(passwordField, 1, 1); // second row, second column
+        GridPane.setConstraints(login, 0, 2); // third row, first column
+        centerLayout.getChildren().setAll(usernameField, usernameLabel, passwordField, passwordLabel, login); // adds all of the objects to the GridPane
+        layout.setCenter(centerLayout); // adds the layout to the BorderPAne
+
+        // Scene
+        Scene mainScene = new Scene(layout, WIDTH, HEIGHT);
+        mainScene.getStylesheets().add("/com/groupproject/nik/Resources/master.css");
 
         // Main stage options
         // set the window's title
         stage.setTitle(windowTitle);
+        // set the scene
+        stage.setScene(mainScene);
+        stage.show();
     }
 }
---
+++ src/com/groupproject/nik/Model/ProductDetails.java
@@ -0,0 +1,6 @@
+package com.groupproject.nik.Model;
+
+public class ProductDetails {
+    private String name;
+    private double weight; // we'll have to decide what unit this is measured in
+}
