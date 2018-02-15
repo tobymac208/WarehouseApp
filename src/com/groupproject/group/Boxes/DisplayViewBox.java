3e56a9d7dfeebdf550adab2a60cded67e85c3f35
Nik <ironman_fern@comcast.net>
2018-02-15 17:45:06 -0600

    Updated display method in DisplayViewBox class. Comments for new updates are in the method

--- src/com/groupproject/group/Boxes/DisplayViewBox.java
+++ src/com/groupproject/group/Boxes/DisplayViewBox.java
@@ -1,6 +1,10 @@
 package com.groupproject.group.Boxes;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -12,39 +16,55 @@ import java.util.ArrayList;
  */
 public class DisplayViewBox {
     private static String windowTitle;
+    private static Stage window;
     /** This method opens a new window that allows a user to see a bunch of populated information on the screen.
      * Pass in a a list of labels and descriptions (parallel arrays), to have them populate on the screen */
     public static void display(String title, ArrayList<String> labels, ArrayList<String> descriptions){
-        windowTitle = title; // Change this to change the title of the window
-        // Create the new window
-        Stage window = new Stage();
-
-        /* Scene options */
-        // Objects
-
-        /* Add labels to the list view */
-        ListView<String> labelListView = new ListView<>();
-        // run a for-each loop to populate the listView
-        for(String currentString : labels){ // adds the current string to the listView
-            labelListView.getItems().add(currentString);
+        // check if the labels and descriptions list have the same amount of items to avoid problems
+        if(labels.size() != descriptions.size()){
+            System.out.println("ArrayLists MUST have the same amount of objects for both. Cannot open window!");
         }
+        else{ // show and populate the screen
+            windowTitle = title; // Change this to change the title of the window
+            // Create the new window
+            window = new Stage();
 
-        // Events
-        // listener for when the user clicks on the items of the listView, since clicking doesn't trigger an event
-        labelListView.getSelectionModel().selectedItemProperty().addListener((model, oldVal, newVal) -> {
-            // Get the index of the item clicked
-            int index = labelListView.getSelectionModel().getSelectedIndex();
-            // analyze the index and set the TextArea at the bottom with the corresponding text of the field selected -- this is where parallel arrays come into play
-            String currentInfo = descriptions.get(index);
-            // set the TextArea with the currentInfo that was retrieved from seeing which index was selected
-            // TODO: Change the TextArea object's text to currentInfo
-        });
+            /* Scene options */
+            /* Objects */
+            /* Add labels to the list view */
+            ListView<String> labelListView = new ListView<>();
+            // run a for-each loop to populate the listView
+            for(String currentString : labels){ // adds the current string to the listView
+                labelListView.getItems().add(currentString); // adds the current string into the listView
+            }
 
-        // Scene
+            // TextArea object for display the current description for the current item
+            TextArea descriptionArea = new TextArea("");
 
-        // Window options
-        window.setTitle(windowTitle); // sets the title of the window
-        window.initModality(Modality.APPLICATION_MODAL); // this restricts users from clicking on another window until this one is closed
-        window.showAndWait(); // call this method for every window that isn't the main login screen
-    }
-}
+            // Events
+            // listener for when the user clicks on the items of the listView, since clicking doesn't trigger an event
+            labelListView.getSelectionModel().selectedItemProperty().addListener((model, oldVal, newVal) -> {
+                // Get the index of the item clicked
+                int index = labelListView.getSelectionModel().getSelectedIndex();
+                // analyze the index and set the TextArea at the bottom with the corresponding text of the field selected -- this is where parallel arrays come into play
+                String currentInfo = descriptions.get(index);
+                // set the TextArea with the currentInfo that was retrieved from seeing which index was selected
+                descriptionArea.setText(currentInfo);
+            });
+
+            // Layout -- Creates a layout, sets the spacing in the constructor, sets the alignment, and then adds the two objects to the layout
+            VBox layout = new VBox(20);
+            layout.setAlignment(Pos.CENTER);
+            layout.getChildren().addAll(labelListView, descriptionArea); // add the listView and the textArea to the screen
+
+            // Scene -- Creates a scene, and then sets the layout to use and the width and height of the window
+            Scene mainScene = new Scene(layout, 600, 400);
+
+            // Window options
+            window.setTitle(windowTitle); // sets the title of the window
+            window.setScene(mainScene);
+            window.initModality(Modality.APPLICATION_MODAL); // this restricts users from clicking on another window until this one is closed
+            window.showAndWait(); // call this method for every window that isn't the main login screen
+        } // end of else
+    } // end of display()
+} // end of class DisplayViewBox
