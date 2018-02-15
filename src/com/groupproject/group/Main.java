95b9f714b63b3275653c23c42e1b92079d529cfa
Nik <ironman_fern@comcast.net>
2018-02-15 17:52:26 -0600

    Completed, and tested, the DisplayViewBox. Users can now choose to see the about page on the main login page.

--- src/com/groupproject/group/Main.java
+++ src/com/groupproject/group/Main.java
@@ -1,5 +1,6 @@
 package com.groupproject.group;
 
+import com.groupproject.group.Boxes.DisplayViewBox;
 import com.groupproject.group.Model.AccountsList;
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -10,7 +11,10 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 /*
  * Authors: Nik, Scott, Joe
@@ -35,6 +39,9 @@ public class Main extends Application {
         // TODO: Place objects here
         // NOTE: All objects placed here are for a skeleton setup, and should be modified for a better user experience
 
+        // TOP objects
+        Button about = new Button("About"); // about button
+
         // CENTER objects
         // Username objects
         Label usernameLabel = new Label("Username:");
@@ -52,9 +59,21 @@ public class Main extends Application {
             // open window for displaying settings
             System.out.println("Functionality needs to be created for this to work.");
         });
+        about.setOnAction(event -> {
+            ArrayList<String> labels = new ArrayList<>(), information = new ArrayList<>(); // creates and initializes two new ArrayList objects
+            // Populate the labels list
+            labels.add("What is this program?");
+            // Populate the information list
+            information.add("This program is for allowing a Warehouse to organize and manage its stock.");
+            DisplayViewBox.display("About", labels, information); // calls the method to show the window
+        });
 
         // Layout -- the place to put all of the objects
         BorderPane layout = new BorderPane();
+        // TOP
+        // Creates a horizontal box layout for the top of the window -- this includes things like Settings and Quit buttons
+        HBox topLayout = new HBox(20);
+        topLayout.getChildren().add(about);
         // CENTER
         GridPane centerLayout = new GridPane();
         centerLayout.setAlignment(Pos.CENTER); // SETS WHERE THE LOGIN OPTIONS ARE ALIGNED ON THE SCREEN
@@ -66,7 +85,9 @@ public class Main extends Application {
         GridPane.setConstraints(passwordField, 1, 1); // second row, second column
         GridPane.setConstraints(login, 0, 2); // third row, first column
         centerLayout.getChildren().setAll(usernameField, usernameLabel, passwordField, passwordLabel, login); // adds all of the objects to the GridPane
-        layout.setCenter(centerLayout); // adds the layout to the BorderPAne
+        // Add the layouts to the main BorderPane layout
+        layout.setTop(topLayout); // adds the TOP layout to the BorderPane
+        layout.setCenter(centerLayout); // adds the CENTER layout to the BorderPane
 
         // Scene -- creates a scene and specifies its WIDTH and HEIGHT (these two constants can found at the top of this file, as a member variable)
         Scene mainScene = new Scene(layout, WIDTH, HEIGHT);
